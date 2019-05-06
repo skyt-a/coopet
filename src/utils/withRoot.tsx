6e7a9323f6e27a21b4d84c054680b8a80925fcdc
Sakou Yuta <yuyu801065@gmail.com>
2019-05-06 14:47:16 +0900

    material-ui(旧@material-ui)への依存関係を削除 material-ui(旧@material-ui)関連のソースを修正

--- src/utils/withRoot.tsx
+++ src/utils/withRoot.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// 任意の Theme Colors
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { red, pink } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
-    // Theme Colors
+    // テーマ色設定
     palette: {
+        type: 'dark',
         primary: red,
         secondary: pink,
     },
-    // typography
+    // タイポグラフィ設定
     typography: {
         useNextVariants: true
     }
@@ -18,6 +18,7 @@ const theme = createMuiTheme({
 
 function withRoot<P>(Component: React.ComponentType<P>) {
     function WithRoot(props: P) {
+        // CssBaseLineによってブラウザ感
         return (
             <MuiThemeProvider theme={theme}>
                 <CssBaseline />
